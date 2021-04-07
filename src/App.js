ff6351b30b4c89d0d935df309b1bc09d647d6361
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-07 11:38:03 +0530

    setting up index page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Planets from './components/Planets';
+import People from './components/People';
+import Navbar from './components/Navbar';
 
 function App() {
+
+  const [page, setPage] = useState('planets');
+
   return (
     <div className="App">
-
+      <h1>Star Wars Info</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        {page === 'planets' ? <Planets /> : <People />}
+      </div>
     </div>
   );
 }
