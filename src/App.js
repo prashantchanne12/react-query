d444dec0fda0d81e9c76fc358a06c8641295955b
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-08 17:17:51 +0530

    react-query dev tools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import React, { useState } from 'react';
 import Planets from './components/Planets';
 import People from './components/People';
 import Navbar from './components/Navbar';
@@ -11,15 +12,18 @@ function App() {
   const [page, setPage] = useState('planets');
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Star Wars Info</h1>
-        <Navbar setPage={setPage} />
-        <div className="content">
-          {page === 'planets' ? <Planets /> : <People />}
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Star Wars Info</h1>
+          <Navbar setPage={setPage} />
+          <div className="content">
+            {page === 'planets' ? <Planets /> : <People />}
+          </div>
         </div>
-      </div>
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 }
 
